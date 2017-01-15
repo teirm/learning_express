befc36f91b876c1de3943efd75a2a86259516d23
teirm <tribunus.vii@gmail.com>
2017-01-14 19:34:13 -0500

    Writing Middleware

--- myapp/app.js
+++ myapp/app.js
@@ -1,14 +1,40 @@
 var express = require('express')
 var app = express()
 
+var myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+}
+
+var requestTime = function (req, res, next) {
+    req.requestTime = Date.now()
+    next()
+}
+
+/* Have to load middleware */
+/* Middleware loaded first is executed first */
+app.use(myLogger)
+app.use(requestTime)
+
 app.get('/', function (req, res){
-    res.send('Hello World!')
+    var responseText  = 'Hello World!<br>'
+    responseText += '<small>Requested at: '
+                    + req.requestTime
+                    + '</small>'
+
+    res.send(responseText)
 })
 
+
+
+/*
 app.get('/user', function (req, res) {
     res.send('Got a PUT request at /user')
 })
+*/
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(3000)
+
+/*
+    app.use(express.static('public'))
+*/
